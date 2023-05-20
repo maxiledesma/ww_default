1333ca8c6526b92677b13d28b66f64c901715eb6
Maximiliano Ledesma <maximilianoledesma@gmail.com>
2023-05-19 22:35:22 -0300

    Created custom slider extension using Slick

---
+++ app/code/Deviget/CustomSlider/view/frontend/requirejs-config.js
@@ -0,0 +1,15 @@
+var config = {
+    map: {
+        '*': {
+            customSlider: 'Deviget_CustomSlider/js/slider',
+        }
+    },
+    paths: {
+        slick: 'Deviget_CustomSlider/js/slick.min'
+    },
+    shim: {
+        slick: {
+            deps: ['jquery']
+        }
+    }
+};
